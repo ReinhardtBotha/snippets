628b5556326d1a8279702c68f82ccadee6b53756
Reinhardt Botha <botha.wr@gmail.com>
2024-09-24 13:51:56 -0700

    feat: caching setup

--- actions/index.ts
+++ actions/index.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { db } from "@/db";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function editSnippet(id: number, code: string) {
@@ -9,6 +10,7 @@ export async function editSnippet(id: number, code: string) {
     data: { code },
   });
 
+  revalidatePath(`/snippets/${id}`);
   redirect(`/snippets/${id}`);
 }
 
@@ -17,6 +19,7 @@ export async function deleteSnippet(id: number) {
     where: { id },
   });
 
+  revalidatePath("/");
   redirect(`/`);
 }
 
@@ -24,30 +27,43 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  // Check the user's inputs and make sure they're valid
-  const title = formData.get("title");
-  const code = formData.get("code");
+  try {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-  if (typeof title !== "string" || title.length < 3) {
-    return {
-      message: "Title must be longer",
-    };
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
   }
 
-  if (typeof code !== "string" || code.length < 10) {
-    return {
-      message: "Code must be longer",
-    };
-  }
-
-  // Create a new record in the database
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
-
+  revalidatePath("/");
   // Redirect the user back to the root route
   redirect("/");
 }
